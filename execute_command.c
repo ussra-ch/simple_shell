521a371c1cc381f6e1d266b6629b4c6a613c29d6
Yousra Chatouane <yousra.ch.etudiante@gmail.com>
2023-11-16 00:40:41 +0100

    done

--- execute_command.c
+++ execute_command.c
@@ -6,15 +6,15 @@
  * @argv: An array of strings representing the program arguments
  * Return: the exit status of the child process, -1 (error)
  */
-int execute_command(char **commandArgs, char **argv) /* , char **argv, int idx*/
+int execute_command(char **commandArgs, char **argv, int idx)
 {
-	/*char *cmd;
+	char *cmd;
 	char p[] = "PATH";
 	pid_t child;
 	int status, check;
 
 	check = shell_strcmp(commandArgs[0], p);
-	 check if the input is PATH for task 3
+	 /*check if the input is PATH for task 3*/
 	if (check == 0)
 	{
 		cmd = _getpath(commandArgs[0]);
@@ -24,7 +24,7 @@ int execute_command(char **commandArgs, char **argv) /* , char **argv, int idx*/
 			return (0);
 		}
 	
-	 Check if the entered command is "exit"
+	 /*Check if the entered command is "exit"*/
 		if (shell_strcmp(commandArgs[0], "exit") == 0)
 		{
 			free(cmd);
@@ -52,61 +52,7 @@ int execute_command(char **commandArgs, char **argv) /* , char **argv, int idx*/
 		perror("fork");
 		return (-1);
 	}
-	return (-1);*/
-	pid_t pid = fork();
-	int status;
-	char *pa, *token, *dir;
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) { // Child process
-        char *args[64];
-        int i = 0;
-
-        token = strtok(commandArgs[0], " \t\n");
-
-        while (token != NULL) {
-            args[i++] = token;
-            token = strtok(NULL, " \t\n");
-        }
-
-        args[i] = NULL;
-
-        // Check if the command includes a path separator '/'
-        if (strchr(args[0], '/') != NULL) {
-            // If it does, try to execute it directly
-            execv(args[0], args);
-            perror("execv");
-            exit(EXIT_FAILURE);
-        }
-// Get the PATH environment variable
-		pa = _getenv("PATH");
-        if (pa == NULL) {
-            perror("_getenv");
-            exit(EXIT_FAILURE);
-        }
-        // Tokenize the PATH
-        dir = strtok(pa, ":");
-        while (dir != NULL) {
-            // Construct the full path to the executable
-            char full_path[256]; // Adjust the size accordingly
-            snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[0]);
-            // Check if the executable exists
-            if (access(full_path, X_OK) == 0) {
-                execv(full_path, args);
-                perror("execv");
-                exit(EXIT_FAILURE);
-            }
-            dir = strtok(NULL, ":");
-        }
-        // If the loop completes, the executable was not found
-        //fprintf(stderr, "Command not found: %s\n", args[0]);
-		if (execve(commandArgs[0], commandArgs, environ) == -1)
-			perror(argv[0]);
-        exit(EXIT_FAILURE);
-    } else { // Parent proces
-        return (waitpid(pid, &status, 0));
-    }
+	return (-1);
 }
 
 /**
